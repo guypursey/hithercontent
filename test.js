7e9eb013e92dc9109481571686f9c51adcbe087e
Guy Pursey <guypursey@gmail.com>
2015-10-11 21:42:10 +0100

    Added code for getting project summary from GatherContent.
    Just testing so this gets the info as JSON via an API call and
    the stores a prettified version of it in a file called
    `output.json`.

--- test.js
+++ test.js
@@ -1,2 +1,36 @@
 var https = require("https"),
-  fs = require ("fs");
+  fs = require ("fs"),
+  auth = JSON.parse(fs.readFileSync("_auth.json", { "encoding": "utf8" }));
+
+var options = {
+  auth: auth.user + ":" + auth.pass,
+  headers: {
+    "Accept": "application/vnd.gathercontent.v0.5+json"
+  },
+  host: "api.gathercontent.com",
+  path: "/items?project_id=" + auth.project
+};
+
+var req = https.get(options, function (res) {
+
+  var body = "";
+
+  console.log(res.statusCode);
+
+  res.on("data", function (d) {
+    body += d;
+  });
+
+  res.on("end", function () {
+    var d = JSON.parse(body);
+    var c = JSON.stringify(d, null, "\t");
+    fs.writeFile("output.json", c, { "encoding": "utf8" }, function (d, e) {
+      if (e) throw e;
+    })
+  });
+
+});
+
+req.on("error", function (e) {
+  console.log(e);
+})
