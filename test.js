a5b7d207eda15a01a3e2bb98db77db15eedc5c7b
Guy Pursey <guypursey@gmail.com>
2015-10-12 19:14:11 +0100

    Introduced function for reducing item data to keys and values.
    The new function essentially simplifies the data for use with a
    Mustache or Handlebars templates, getting rid of anything
    extraneous.
    
    I've kept the raw JSON output for each item in, for reference, but
    these are now written to a folder called `raw`, whereas `items` is
    now the reduced data form.

--- test.js
+++ test.js
@@ -44,14 +44,41 @@ var getJSONfromAPI = function (request, output, callback) {
 
 };
 
+
+
 var items = {};
+
+var processItem = function (d) {
+  d.data.config.forEach(function (v, i, a) {
+    var tab_label = v.label;
+    v.elements.forEach(function (v, i, a) {
+      var k = tab_label + "_" + v.label;
+      k = k && k.replace(/\s/g, "-");
+      if (v.type === "text") {
+        items[d.data.id][k] = v.value;
+      } else if (v.type === "choice_checkbox" || v.type === "choice_radio") {
+        items[d.data.id][k] = v.options.filter(function (v, i, a) {
+          return v.selected;
+        }).map(function (v, i, a) {
+          return v.label;
+        });
+      }
+    });
+  });
+
+  fs.writeFile("items/" + d.data.id + ".json", JSON.stringify(items[d.data.id], null, "\t"), { "encoding": "utf8" }, function (c, e) {
+    if (e) throw e;
+    console.log("file written", d.data.id);
+  });
+};
+
 
 var items_cb = function (d) {
   var i = d.data.length;
   while (i) {
     i--;
-    items[d.data[i].id] = d.data[i];
-    getJSONfromAPI("/items/" + d.data[i].id, "items/" + d.data[i].id + ".json");
+    items[d.data[i].id] = {};
+    getJSONfromAPI("/items/" + d.data[i].id, "raw/" + d.data[i].id + ".json", processItem);
   }
 
   fs.writeFile("sorted_output.json", JSON.stringify(items, null, "\t"), { "encoding": "utf8" }, function (d, e) {
