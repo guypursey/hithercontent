d3323202b652b73f485fbefdb547a7bff33ae5fb
Guy Pursey <guypursey@gmail.com>
2015-10-12 09:29:52 +0100

    Created a general-purpose function for processing JSON from API.
    At the moment the function takes just two string arguments:
    
     - A request string containing the path of the GET call.
     - An output string specifying which file the JSON should be
    written to.

--- test.js
+++ test.js
@@ -1,36 +1,43 @@
 var https = require("https"),
   fs = require ("fs"),
-  auth = JSON.parse(fs.readFileSync("_auth.json", { "encoding": "utf8" }));
+  auth = JSON.parse(fs.readFileSync("_auth.json", { "encoding": "utf8" })),
+  request = "/items?project_id=" + auth.project;
 
-var options = {
-  auth: auth.user + ":" + auth.pass,
-  headers: {
-    "Accept": "application/vnd.gathercontent.v0.5+json"
-  },
-  host: "api.gathercontent.com",
-  path: "/items?project_id=" + auth.project
+var getJSONfromAPI = function (request, output) {
+
+  var options = {
+    auth: auth.user + ":" + auth.pass,
+    headers: {
+      "Accept": "application/vnd.gathercontent.v0.5+json"
+    },
+    host: "api.gathercontent.com",
+    path: request
+  };
+
+  var req = https.get(options, function (res) {
+
+    var body = "";
+
+    res.on("data", function (d) {
+      body += d;
+    });
+
+    res.on("end", function () {
+      var d = JSON.parse(body);
+      var c = JSON.stringify(d, null, "\t");
+      if (output && typeof output === "string") {
+        fs.writeFile(output, c, { "encoding": "utf8" }, function (d, e) {
+          if (e) throw e;
+        });
+      }
+    });
+
+  });
+
+  req.on("error", function (e) {
+    console.log(e);
+  });
+
 };
 
-var req = https.get(options, function (res) {
-
-  var body = "";
-
-  console.log(res.statusCode);
-
-  res.on("data", function (d) {
-    body += d;
-  });
-
-  res.on("end", function () {
-    var d = JSON.parse(body);
-    var c = JSON.stringify(d, null, "\t");
-    fs.writeFile("output.json", c, { "encoding": "utf8" }, function (d, e) {
-      if (e) throw e;
-    })
-  });
-
-});
-
-req.on("error", function (e) {
-  console.log(e);
-})
+getJSONfromAPI(request, "output.json");
