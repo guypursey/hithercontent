e7e648c8418350e138acaaa515a976c07e691344
Guy Pursey <guypursey@gmail.com>
2015-10-15 19:37:32 +0100

    Separated all the main code out into `index.js` for modularisation.
    By separating out the code, I've prepared the way for making the
    code usable as an NPM package.
    
    I've relabelled the name of the module in `package.json` as
    "GatherContent" as the name isn't taken yet and eventually the code
    should not be limited to one particular function of GatherContent..

---
+++ index.js
@@ -0,0 +1,75 @@
+var https = require("https");
+
+module.exports = (function () {
+
+  var auth = {};
+
+  var init = function (cred) {
+    if (typeof cred === "object") {
+      if (cred.hasOwnProperty("user") && typeof cred.user === "string") {
+        auth.user = cred.user;
+      }
+      if (cred.hasOwnProperty("akey") && typeof cred.akey === "string") {
+        auth.akey = cred.akey;
+      }
+    }
+  }
+
+  var getJSONfromAPI = function (request, callback) {
+
+    var options = {
+      auth: auth.user + ":" + auth.akey,
+      headers: {
+        "Accept": "application/vnd.gathercontent.v0.5+json"
+      },
+      host: "api.gathercontent.com",
+      path: request
+    };
+
+    var req = https.get(options, function (res) {
+
+      var body = "";
+
+      res.on("data", function (chunk) {
+        body += chunk;
+      });
+
+      res.on("end", function () {
+        var data = JSON.parse(body);
+        if (callback && typeof callback === "function") {
+          callback(data);
+        }
+      });
+
+    });
+
+    req.on("error", function (e) {
+      console.log(e);
+    });
+
+  };
+
+  var reduceItemToKVPairs = function (d) {
+    var item = {};
+    d.data.config.forEach(function (v, i, a) {
+      var tab_label = v.label;
+      v.elements.forEach(function (v, i, a) {
+        var k = tab_label + "_" + v.label;
+        k = k && k.replace(/\s/g, "-");
+        if (v.type === "text") {
+          item[k] = v.value;
+        } else if (v.type === "choice_checkbox" || v.type === "choice_radio") {
+          item[k] = v.options.filter(v => v.selected).map(v => v.label);
+        }
+      });
+    });
+    return item;
+  };
+
+  return {
+    init: init,
+    getJSONfromAPI: getJSONfromAPI,
+    reduceItemToKVPairs: reduceItemToKVPairs,
+  }
+
+}());
--- test.js
+++ test.js
@@ -1,62 +1,10 @@
-var https = require("https"),
-  fs = require("fs"),
+var fs = require("fs"),
+  gathercontent = require("./index.js"),
   auth = JSON.parse(fs.readFileSync("_auth.json", { "encoding": "utf8" })),
   request = "/items?project_id=" + auth.project,
   source = fs.readFileSync("./layouts/default.html", { "encoding": "utf8" }),
   handlebars = require("handlebars"),
   layout = handlebars.compile(source);
-
-var getJSONfromAPI = function (request, callback) {
-
-  var options = {
-    auth: auth.user + ":" + auth.pass,
-    headers: {
-      "Accept": "application/vnd.gathercontent.v0.5+json"
-    },
-    host: "api.gathercontent.com",
-    path: request
-  };
-
-  var req = https.get(options, function (res) {
-
-    var body = "";
-
-    res.on("data", function (chunk) {
-      body += chunk;
-    });
-
-    res.on("end", function () {
-      var data = JSON.parse(body);
-      if (callback && typeof callback === "function") {
-        callback(data);
-      }
-    });
-
-  });
-
-  req.on("error", function (e) {
-    console.log(e);
-  });
-
-};
-
-var reduceItemToKVPairs = function (d) {
-  var item = {};
-  d.data.config.forEach(function (v, i, a) {
-    var tab_label = v.label;
-    v.elements.forEach(function (v, i, a) {
-      var k = tab_label + "_" + v.label;
-      k = k && k.replace(/\s/g, "-");
-      if (v.type === "text") {
-        item[k] = v.value;
-      } else if (v.type === "choice_checkbox" || v.type === "choice_radio") {
-        item[k] = v.options.filter(v => v.selected).map(v => v.label);
-      }
-    });
-  });
-  return item;
-};
-
 
 // sample code using functions above
 
@@ -64,7 +12,7 @@ var items = {};
 
 var processItem = function (d) {
 
-  var item = reduceItemToKVPairs(d);
+  var item = gathercontent.reduceItemToKVPairs(d);
 
   // write raw JSON for each item to its own file in a `raw` folder
   fs.writeFile("raw/" + d.data.id + ".json", JSON.stringify(d, null, "/t"), { "encoding": "utf8" }, function (c, e) {
@@ -91,8 +39,9 @@ var processAllItems = function (d) {
   while (i) {
     i--;
     items[d.data[i].id] = {};
-    getJSONfromAPI("/items/" + d.data[i].id, processItem);
+    gathercontent.getJSONfromAPI("/items/" + d.data[i].id, processItem);
   }
 };
 
-getJSONfromAPI(request, processAllItems);
+gathercontent.init(auth);
+gathercontent.getJSONfromAPI(request, processAllItems);
