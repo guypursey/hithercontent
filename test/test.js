1772ddd4c8b01d46ce4905384fdadcd886927436
Guy Pursey <guypursey@gmail.com>
2015-10-18 18:00:24 +0100

    Added `test.js` for Mocha BDD style interface with Chai expect.
    This mostly tests API calls through getJSONfromAPI function.
    
    Although Mocha's asynchronous test handling works well here, I'm
    not yet clear on how to ensure the authentication works without
    committing specific details (which should stay out of the
    repository).

---
+++ test/test.js
@@ -0,0 +1,39 @@
+var fs = require("fs"),
+    hc_path = "../index.js",
+    hithercontent = require(hc_path),
+    auth = JSON.parse(fs.readFileSync("_auth.json", { "encoding": "utf8" })),
+    expect = require("chai").expect;
+
+describe("Getting JSON from API", function() {
+    describe("Requesting content without initialisation", function () {
+        it("should inform the user that the `hithercontent` has not been properly initialised", function (done) {
+            hithercontent.getJSONfromAPI("/items/0", function (data) {
+                expect(data).to.be.an("object");
+                done();
+            });
+        })
+    });
+
+    describe("Requesting content from an incorrect path", function () {
+        it("should inform the user that an incorrect path has been provided", function (done) {
+            hithercontent.init(auth);
+            hithercontent.getJSONfromAPI("/items/0", function (data) {
+                console.log("False path");
+                console.log(data);
+                expect(data).to.be.an("object");
+                done();
+            });
+        });
+    });
+
+    describe("Requesting content from a correct path", function () {
+        it("should return a usable data object", function (done) {
+            hithercontent.getJSONfromAPI("/items/1512999", function (data) {
+                console.log("Real data");
+                console.log(data);
+                expect(data).to.be.an("object");
+                done();
+            });
+        })
+    });
+});
