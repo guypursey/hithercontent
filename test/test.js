13d3e410336b60686abbe157d0d595b2c1c23d67
Guy Pursey <guypursey@gmail.com>
2015-10-19 17:36:00 +0100

    Tidied up `test/test.js` for API tests.
    There is still some work to do around the initialisation test.

--- test/test.js
+++ test/test.js
@@ -5,7 +5,7 @@ var fs = require("fs"),
     expect = require("chai").expect;
 
 describe("Getting JSON from API", function() {
-    describe("Requesting content without initialisation", function () {
+    describe("without initialisation", function () {
         it("should inform the user that the `hithercontent` has not been properly initialised", function (done) {
             hithercontent.getJSONfromAPI("/items/0", function (data) {
                 expect(data).to.be.an("object");
@@ -14,23 +14,19 @@ describe("Getting JSON from API", function() {
         })
     });
 
-    describe("Requesting content from an incorrect path", function () {
+    describe("using an incorrect path", function () {
         it("should inform the user that an incorrect path has been provided", function (done) {
             hithercontent.init(auth);
             hithercontent.getJSONfromAPI("/items/0", function (data) {
-                console.log("False path");
-                console.log(data);
-                expect(data).to.be.an("object");
+                expect(data).to.be.an("array");
                 done();
             });
         });
     });
 
-    describe("Requesting content from a correct path", function () {
+    describe("from a correct path", function () {
         it("should return a usable data object", function (done) {
             hithercontent.getJSONfromAPI("/items/1512999", function (data) {
-                console.log("Real data");
-                console.log(data);
                 expect(data).to.be.an("object");
                 done();
             });
