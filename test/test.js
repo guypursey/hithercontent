d1da9b0e51ed0848d7ecd4cb307ae0ae762f2e4c
Guy Pursey <guypursey@gmail.com>
2015-10-25 18:11:05 +0000

    Moved mocking of server to a set-up function.
    All the Sinon-related code is now handled before each Mocha test
    is run. This mocks the GatherContent API in a very basic way at the
    moment and not accurately -- authentication is checked and if it
    passes then the `options` argument stringified to the callback,
    rather than a mock-data set.
    
    Paths are not checked yet, although if authentication fails
    the message "Invalid credentials." is passed to the callback
    which does reflect GatherContent's current behaviour.

--- test/test.js
+++ test/test.js
@@ -3,33 +3,37 @@ var fs = require("fs"),
     hithercontent = require(hc_path),
     auth = JSON.parse(fs.readFileSync("_auth.json", { "encoding": "utf8" })),
     expect = require("chai").expect,
-    http = require("http"),
+    https = require("https"),
     sinon = require("sinon"),
     PassThrough = require("stream").PassThrough;
 
 describe("Getting JSON from API", function() {
 
-    beforeEach(function () {
-        this.request = sinon.stub(http, "request");
+    beforeEach("using mock API to approximate GatherContent", function () {
+        //this.get = sinon.stub(https, "get");
+        this.get = sinon.stub(https, "get", function (options, callback) {
+            var request = new PassThrough(),
+                response = new PassThrough(),
+                auth_check = auth.user + ":" + auth.akey;
+            if (typeof callback === "function") {
+                callback(response);
+            }
+            if (options.hasOwnProperty("auth") && (options.auth === auth_check)) {
+                response.write(JSON.stringify(options));
+            } else {
+                response.write("Invalid credentials.")
+            }
+            response.end();
+            return request;
+        });
     });
 
-    afterEach(function () {
-        http.request.restore();
+    afterEach("restoring real API call", function () {
+        https.get.restore();
     });
 
     describe("without initialisation", function () {
         it("should inform the user that the `hithercontent` has not been properly initialised", function (done) {
-            var expected = { "hello": "world" },
-                response = new PassThrough(),
-                request = new PassThrough();
-
-            response.write(JSON.stringify(expected));
-            response.end();
-
-            this.request
-                .callsArgWith(1, response)
-                .returns(request);
-
             hithercontent.getJSONfromAPI("/items/0", function (data) {
                 expect(data).to.be.an("object");
                 done();
@@ -39,7 +43,6 @@ describe("Getting JSON from API", function() {
 
     describe.skip("using an incorrect path", function () {
         it("should inform the user that an incorrect path has been provided", function (done) {
-            hithercontent.init(auth);
             hithercontent.getJSONfromAPI("/items/0", function (data) {
                 expect(data).to.be.an("object");
                 done();
@@ -47,8 +50,9 @@ describe("Getting JSON from API", function() {
         });
     });
 
-    describe.skip("from a correct path", function () {
+    describe("from a correct path", function () {
         it("should return a usable data object", function (done) {
+            hithercontent.init(auth);
             hithercontent.getJSONfromAPI("/items/1512999", function (data) {
                 expect(data).to.be.an("object");
                 done();
