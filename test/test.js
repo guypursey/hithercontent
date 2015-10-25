677c820eb10094579e5bf8e45bd3ea51eda8fbab
Guy Pursey <guypursey@gmail.com>
2015-10-25 14:49:11 +0000

    Added code to test API functions without making real calls.
    Still some work to do on this so am trying with one unit test for
    now and have temporarily marked the others to be skipped.
    
    Using Sinon.js, the server call within `getJSONfromAPI` function
    is simulated for the purposes of a unit test.

--- test/test.js
+++ test/test.js
@@ -2,11 +2,34 @@ var fs = require("fs"),
     hc_path = "../index.js",
     hithercontent = require(hc_path),
     auth = JSON.parse(fs.readFileSync("_auth.json", { "encoding": "utf8" })),
-    expect = require("chai").expect;
+    expect = require("chai").expect,
+    http = require("http"),
+    sinon = require("sinon"),
+    PassThrough = require("stream").PassThrough;
 
 describe("Getting JSON from API", function() {
+
+    beforeEach(function () {
+        this.request = sinon.stub(http, "request");
+    });
+
+    afterEach(function () {
+        http.request.restore();
+    });
+
     describe("without initialisation", function () {
         it("should inform the user that the `hithercontent` has not been properly initialised", function (done) {
+            var expected = { "hello": "world" },
+                response = new PassThrough(),
+                request = new PassThrough();
+
+            response.write(JSON.stringify(expected));
+            response.end();
+
+            this.request
+                .callsArgWith(1, response)
+                .returns(request);
+
             hithercontent.getJSONfromAPI("/items/0", function (data) {
                 expect(data).to.be.an("object");
                 done();
@@ -14,7 +37,7 @@ describe("Getting JSON from API", function() {
         })
     });
 
-    describe("using an incorrect path", function () {
+    describe.skip("using an incorrect path", function () {
         it("should inform the user that an incorrect path has been provided", function (done) {
             hithercontent.init(auth);
             hithercontent.getJSONfromAPI("/items/0", function (data) {
@@ -24,7 +47,7 @@ describe("Getting JSON from API", function() {
         });
     });
 
-    describe("from a correct path", function () {
+    describe.skip("from a correct path", function () {
         it("should return a usable data object", function (done) {
             hithercontent.getJSONfromAPI("/items/1512999", function (data) {
                 expect(data).to.be.an("object");
