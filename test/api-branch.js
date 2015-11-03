78b2ff30d1ed04761ff5e5577196e3a1a2e09361
Guy Pursey <guypursey@gmail.com>
2015-11-03 20:21:29 +0000

    Added new file for unit tests for a new feature.
    The new branch selector feature should enable automatic selection
    of a branch of a project from GatherContent, from the root to an
    individual item and anything in between.

---
+++ test/api-branch.js
@@ -0,0 +1,38 @@
+var fs = require("fs"),
+    hc_path = "../index.js",
+    hithercontent = require(hc_path),
+    auth = JSON.parse(fs.readFileSync("_auth.json", { "encoding": "utf8" })),
+    expect = require("chai").expect,
+    https = require("https"),
+    sinon = require("sinon"),
+    PassThrough = require("stream").PassThrough;
+
+describe("Using the branch selector", function() {
+
+    beforeEach("using mock API to approximate GatherContent", function () {
+        this.get = sinon.stub(https, "get", function (options, callback) {
+            var request = new PassThrough(),
+                response = new PassThrough(),
+                auth_check = auth.user + ":" + auth.akey;
+            if (typeof callback === "function") {
+                callback(response);
+            }
+            if (options.hasOwnProperty("auth") && (options.auth === auth_check)) {
+                response.write(JSON.stringify(options));
+            } else {
+                response.write("Invalid credentials.")
+            }
+            response.end();
+            return request;
+        });
+    });
+
+    afterEach("restoring real API call", function () {
+        https.get.restore();
+    });
+
+    it("should be able to fetch the whole project");
+
+    it("should be able to fetch a single branch of the project");
+
+});
