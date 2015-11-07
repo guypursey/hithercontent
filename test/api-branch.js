d81a98913e1be4d84a109bb44505e5c45be2c910
Guy Pursey <guypursey@gmail.com>
2015-11-07 11:16:01 +0000

    Added mock project data to `test/api-branch.js` unit test file.
    This will enable simulation of the GatherContent server, via
    `sinon.js` package, enabling the writing of unit tests for a new
    project branch fetching feature.

--- test/api-branch.js
+++ test/api-branch.js
@@ -7,6 +7,28 @@ var fs = require("fs"),
     sinon = require("sinon"),
     PassThrough = require("stream").PassThrough;
 
+var project_overview = {
+    "data": [
+        { "id": "1", "parent_id": "0" },
+        { "id": "2", "parent_id": "0" },
+        { "id": "1a", "parent_id": "1" },
+        { "id": "1b", "parent_id": "1" },
+        { "id": "2a", "parent_id": "2" },
+        { "id": "2b", "parent_id": "2" },
+        { "id": "2b1", "parent_id": "2b" }
+    ]
+};
+
+var project_content = [
+    "1": { "data": { "config": [ { "label": "First tab", "elements": [ { "type": "text", "label": "Text element", "value": "Lorem ipsum" } ] } ] } },
+    "2": { "data": { "config": [ { "label": "First tab", "elements": [ { "type": "text", "label": "Text element", "value": "Lorem ipsum" } ] } ] } },
+    "1a": { "data": { "config": [ { "label": "First tab", "elements": [ { "type": "text", "label": "Text element", "value": "Lorem ipsum" } ] } ] } },
+    "1b": { "data": { "config": [ { "label": "First tab", "elements": [ { "type": "text", "label": "Text element", "value": "Lorem ipsum" } ] } ] } },
+    "2a": { "data": { "config": [ { "label": "First tab", "elements": [ { "type": "text", "label": "Text element", "value": "Lorem ipsum" } ] } ] } },
+    "2b": { "data": { "config": [ { "label": "First tab", "elements": [ { "type": "text", "label": "Text element", "value": "Lorem ipsum" } ] } ] } },
+    "2b1": { "data": { "config": [ { "label": "First tab", "elements": [ { "type": "text", "label": "Text element", "value": "Lorem ipsum" } ] } ] } }
+]
+
 describe("Using the branch selector", function() {
 
     beforeEach("using mock API to approximate GatherContent", function () {
